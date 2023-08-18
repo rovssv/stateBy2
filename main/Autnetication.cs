3197c87226301cc3b07bc7870e6e11092d1a8ba7
jamessv <jamessrov@gmail.com>
2023-08-18 21:56:11 +0600

    authentication

---
+++ main/Autnetication.cs
@@ -0,0 +1,53 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ForwarderManagementBusinessLayer.Presenters.Users;
+using ForwarderManagementEntityLayer.Users;
+
+namespace FMUnitTest
+{
+    [TestClass]
+    public class Authentication
+    {
+        [TestMethod]
+        public void Login()
+        {
+            UsersPst pst = new UsersPst();
+            string userCode = "oli";
+            bool result = pst.UserExists(userCode);
+            Assert.IsTrue(result == true, "User code exists.");
+            
+            string password = "asdf@1234";
+            result = pst.UserIsAuthenticated(userCode, password);
+            Assert.IsTrue(result == true, "User is authenticated.");
+            
+            UsersDto loggedInUser = pst.LoggedInUser(userCode, password);
+            Assert.IsTrue(loggedInUser != null, "User info found.");
+        }
+
+        [TestMethod]
+        public void ChangeName()
+        {
+            UsersPst pst = new UsersPst();
+            string userCode = "oli";
+            string password = "asdf@1234";
+            UsersDto loggedInUser = pst.LoggedInUser(userCode, password);
+            Assert.IsTrue(loggedInUser != null, "User info found.");
+
+            loggedInUser = pst.ChangeUserDisplayName(loggedInUser, "Md. Oliullah Siddique");
+            Assert.IsTrue(loggedInUser != null, "User display name changed.");
+        }
+
+        [TestMethod]
+        public void ChangePassword()
+        {
+            UsersPst pst = new UsersPst();
+            string userCode = "oli";
+            string password = "asdf@12345";
+            UsersDto loggedInUser = pst.LoggedInUser(userCode, password);
+            Assert.IsTrue(loggedInUser != null, "User info found.");
+
+            loggedInUser = pst.ChangeUserPassword(loggedInUser, "asdf@1234");
+            Assert.IsTrue(loggedInUser != null, "User display name changed.");
+        }
+    }
+}
